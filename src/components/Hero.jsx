1b7fc64c10a2ba36ff0850dbf1df49953f098b5d
carlos daniel <carloscru1dev@gmail.com>
2022-08-23 04:12:16 +0200

    Desbordamiento de pantalla

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import '../App.css';
 
-export const Hero = ({ imageSrc }) => {
+export const Hero = () => {
     return (
         <>
             <div className="hero">
                 <div className='hero__con__img'>
-                    <img src={ imageSrc } alt="hero_image" className="hero__image" />
+                   
                 </div>
                 <h1 className="hero__title"> Image 3D</h1>
             </div>
